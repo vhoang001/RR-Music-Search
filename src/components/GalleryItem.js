2827fb8fc4650ce2a0569342e02b84e87fa1108a
Van Hoang <hoangduykhanhvan@gmail.com>
2024-06-02 10:58:28 -0700

    UPDATE HW 5

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -35,12 +35,22 @@ function GalleryItem(props){
         return (
             <div style={detailStyle}>
                 <h2>{props.item.trackName}</h2>
-                <h3>{props.item.collectionName}</h3>
+                <h3>
+                    <a href={`/artist/${props.item.artistId}`}>
+                        {props.item.artistName}
+                    </a>
+                </h3>
+                <h3>
+                    <a href={`/album/${props.item.collectionId}`}>
+                        {props.item.collectionName}
+                    </a>
+                </h3>
                 <h4>{props.item.primaryGenreName}</h4>
                 <h4>{props.item.releaseDate}</h4>
             </div>
         )
     }
+    
 
     return (
         <div onClick={() =>setView(!view)} style={{'display': 'inline-block'}}>
@@ -51,3 +61,5 @@ function GalleryItem(props){
 }
 
 export default GalleryItem
+
+
